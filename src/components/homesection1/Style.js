dc7476dc6cb7380dab268ae8eb6dc17a7e21d43b
HisRoyalWONGiness <talk2wong@gmail.com>
2020-07-16 15:54:04 +0100

    Worked on responsiveness for the What We Offer Section on Homepage

--- src/components/homesection1/Style.js
+++ src/components/homesection1/Style.js
@@ -3,9 +3,9 @@ import styled from 'styled-components'
 export const Style = styled.div`
     display: flex;
     flex-wrap: wrap;
-    justify-content: center;
+    justify-content: space-between;
     align-items: flex-start;
-    margin: 0em 0em 5em 0em;
+    margin: 0em -2em;
 
     .text_section{
         max-width: 600px;
@@ -39,6 +39,24 @@ export const Style = styled.div`
         width: 700px;
         height: 700px;
         position: relative;
+        margin: 0 2em;
+
+        @media only screen and (max-width: 2000px){
+            width: 600px;
+        }
+
+        @media only screen and (max-width: 1340px){
+            width: 500px;
+        }
+
+        @media only screen and (max-width: 1230px){
+            width: 400px;
+        }
+
+        @media only screen and (max-width: 1200px){
+            width: 100%;
+        }
+
         
         .circle{
             position: absolute;
@@ -47,7 +65,7 @@ export const Style = styled.div`
             width: 700px;
             height: 700px;
             border-radius: 50%;
-            background: #FFFFFF20;
+            background: #FFFFFF10;
             text-align: center;
 
             .bar{
@@ -56,7 +74,7 @@ export const Style = styled.div`
                 right: 0px;
                 width: 105px;
                 height: 700px;
-                background: #FFFFFF20;
+                background: #FFFFFF10;
             }
 
             img{
@@ -64,6 +82,40 @@ export const Style = styled.div`
                 margin: 180px 0;
                 width: 350px;
                 border-radius: 50px;
+            }
+
+            @media only screen and (max-width: 720px){
+                width: 500px;
+                height: 500px;
+
+                .bar{
+                    width: 75px;
+                    height: 500px;
+                }
+
+                img{
+                    margin: 80px 0;
+                    width: 200px;
+                }
+
+
+            }
+
+            @media only screen and (max-width: 470px){
+                width: 350px;
+                height: 350px;
+
+                .bar{
+                    width: 75px;
+                    height: 350px;
+                }
+
+                img{
+                    margin: 80px 0;
+                    width: 200px;
+                }
+
+
             }
         }
     }
