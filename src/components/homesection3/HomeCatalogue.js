c73a98582e4c826f559512ad88462548ab68c987
HisRoyalWONGiness <talk2wong@gmail.com>
2020-07-18 22:04:05 +0100

    Added Catalogue Section on Home

---
+++ src/components/homesection3/HomeCatalogue.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Style} from './Style';
+
+
+function HomeCatalogue (){
+    return(
+        <Style>
+            <div className = 'grand_container'>
+                <h1>Catalogue</h1>
+                <div className = 'container1'>
+                    <div className = 'full1'>
+
+                    </div>
+
+                    <div className = 'with_text'>
+                        <div className = 'image'>
+                            
+                        </div>
+                        <div className = 'text'>
+                            These are available wears for the females who loveAnkara fashion. crazy and enthusiastic about it.<br />
+                            <button> Contact Us</button>
+                        </div>
+                    </div>
+
+                    <div className = 'full2'>
+
+                    </div>
+                </div>
+
+                <div className = 'container2'>
+
+                    <div className = 'halves1'>
+                        <div className = 'halves1_1st'>
+
+                        </div>
+
+                        <div className = 'halves1_2nd'>
+
+                        </div>
+                    </div>
+
+                    <div className = 'full_last_section'>
+                        
+                    </div>
+
+                    <div className = 'halves2'>
+                        <div className = 'halves2_1st'>
+
+                        </div>
+
+                        <div className = 'halves2_2nd'>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </Style>
+    )
+}
+
+export default HomeCatalogue
